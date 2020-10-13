8551d291fdc8b20ad71fb8a50e9110f67c94326b
Godefroy <courtin.godefroy@gmail.com>
2020-10-13 14:43:25 +0200

    essais

--- bridge/club/views.py
+++ bridge/club/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
+
+    # articles = ["1", "2","3"]
+    # context = {
+    #     'articles': articles
+    # }
     return render(request,"club/index.html")
