d541c380da23519e3985a4b795051b7e82feab95
Godefroy <courtin.godefroy@gmail.com>
2020-10-14 10:45:50 +0200

    création des view ok

--- bridge/club/urls.py
+++ bridge/club/urls.py
@@ -3,4 +3,7 @@ from . import views
 app_name ="club"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("article", views.article, name="article"),
+    path("contact", views.contact, name="contact"),
+    path("agenda", views.agenda, name="agenda"),
 ]
