46aa92b6f731b0287cb575e65f78e66398cc2cae
Godefroy <courtin.godefroy@gmail.com>
2020-10-15 10:54:38 +0200

    galerie 80 % ok

--- bridge/club/views.py
+++ bridge/club/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 def index(request):
     return render(request,"club/index.html")
 
-def article(request):
-    return render(request,"club/article.html")
+def article1(request):
+    return render(request,"club/article1.html")
+
+def article2(request):
+    return render(request,"club/article2.html")
+
+def article3(request):
+    return render(request,"club/article3.html")
 
 def contact(request):
     return render(request,"club/contact.html")
@@ -12,8 +18,8 @@ def contact(request):
 def agenda(request):
     return render(request,"club/agenda.html")
 
-def gallerie(request):
-    return render(request,"club/gallerie.html")
+def galerie(request):
+    return render(request,"club/galerie.html")
 
 def evenement(request):
     return render(request,"club/evenement.html")
