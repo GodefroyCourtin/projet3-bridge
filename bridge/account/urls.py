fa3a38588b20365604ebca826babd69e740d1f27
Godefroy <courtin.godefroy@gmail.com>
2020-10-16 12:03:57 +0200

    changement url inscription

--- bridge/account/urls.py
+++ bridge/account/urls.py
@@ -6,9 +6,9 @@ app_name ="account"
 urlpatterns = [
     path("my_account", views.my_account, name="my_account"),
     path(
-        "inscription_producteur",
-        views.inscription_producteur,
-        name="inscription_producteur",
+        "inscription",
+        views.inscription,
+        name="inscription",
     ),
     path("connexion", views.connexion, name="connexion"),
     path("deconnexion", views.deconnexion, name="deconnexion"),
